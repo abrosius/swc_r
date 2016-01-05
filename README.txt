fff120a677adcfc9afbbfcc791ed2ad38e8d1920
User <no@no.com>
2016-01-05 11:48:17 -0500

    added README from challenge 2, some changes to variables.R

---
+++ README.txt
@@ -0,0 +1,2 @@
+This repository is to hold changes and versions of any documents I keep in it. 
+When I commit files to the repository, it will save the previous and current versions. 
--- variables.R
+++ variables.R
@@ -1,3 +1,6 @@
+# This is the document where I learn introductory R. 
+#Jan 5th, 2016
+dist10<- rnorm(10) #random normal distribution of 10 numbers
 x<- 1   #this doesn't execute
 y<- ax*2 #so that you know what you did
 z<- y^2
